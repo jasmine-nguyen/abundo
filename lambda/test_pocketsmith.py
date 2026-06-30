8fc1c9480cd01b01bba9eef599f8032fc10b74ea
Jasmine Nguyen <jasminenguyen.me@gmail.com>
2026-06-30 20:48:05 +1000

    chore: remove pocketsmith

--- lambda/test_pocketsmith.py
+++
@@ -1,6 +0,0 @@
-from pocketsmith import PocketSmithClient
-
-client = PocketSmithClient()
-transactions = client.get_transactions("5256839", "2026-06-20T00:00:00Z")
-
-print(f"transactions:  {transactions}")
